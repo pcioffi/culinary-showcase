219ae371ade3caa9d9ffdfe6e03f1f5279be4a28
pcioffi <pellegrinocioffi93@gmail.com>
2019-10-07 02:53:29 -0400

    Updates

---
+++ precache-manifest.90a5f97318b1d19523d4f6c438012dc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edae14cb4a584e5bd2dce0fa5b9dd3da",
+    "url": "/culinary-showcase/index.html"
+  },
+  {
+    "revision": "e6486dcab1fcf142eb3f",
+    "url": "/culinary-showcase/static/js/2.98a9e70e.chunk.js"
+  },
+  {
+    "revision": "1e308b139f5aca7b6d59",
+    "url": "/culinary-showcase/static/js/main.b83e2f9e.chunk.js"
+  },
+  {
+    "revision": "c9a0d659ef3f076f3b57",
+    "url": "/culinary-showcase/static/js/runtime-main.4d114784.js"
+  },
+  {
+    "revision": "cb6ba5e8988a6c5d51f97227dd9c4d13",
+    "url": "/culinary-showcase/static/media/Culinary-Logo-Text.cb6ba5e8.png"
+  }
+]);
